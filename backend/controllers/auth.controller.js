ef95d6cade95aef75f7a3cb9c70e6cacd6168438
amitraw03 <99679149+amitraw03@users.noreply.github.com>
2024-09-17 23:01:54 +0530

    Day1 setup backend server

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+
+export const signup= async  (req, res) => {   // endpoint for signup
+    res.send('Signup route called');
+}
+export const login= async  (req, res) => {   // endpoint for Login
+    res.send('LogIn route called');
+}
+export const logout= async  (req, res) => {   // endpoint for Logout
+    res.send('LogOut route called');
+}
