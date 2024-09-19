6f55d93429c3ff24406b1eba8159fa2baac14b23
amitraw03 <99679149+amitraw03@users.noreply.github.com>
2024-09-19 20:56:37 +0530

    DAY2: logout controller setup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -9,27 +9,27 @@ const generateTokens = (userId) => {
         { expiresIn: "7d" });
     return { accessToken, refreshToken };
 }
-
+// functn to store refresh token in redis for faster refreshing access token
 const storeRefreshToken = async (userId, refreshToken) => {
     await redis.set(`refresh_token:${userId}`, refreshToken, 'EX', 7 * 24 * 60 * 60); // 7 days
 }
 // functn to make a cookie for the user after signup/registration
 const setCookies = (res, accessToken, refreshToken) => {
-    res.cookie('accessToken' , accessToken, {
+    res.cookie('accessToken', accessToken, {
         httpOnly: true,   // prevent from XSS attacks , cross-site scripting attacks
-        secure: process.env.NODE_ENV === 'production',  
+        secure: process.env.NODE_ENV === 'production',
         sameSite: 'strict',  // prevents CSRF attacks, cross site request forgery attack
         maxAge: 15 * 60 * 1000,  // 15 MINUTES
     });
-    res.cookie('refreshToken' , refreshToken, {
+    res.cookie('refreshToken', refreshToken, {
         httpOnly: true,   // prevent from XSS attacks , cross-site scripting attacks
-        secure: process.env.NODE_ENV === 'production',  
+        secure: process.env.NODE_ENV === 'production',
         sameSite: 'strict',  // prevents CSRF attacks, cross site request forgery attack
         maxAge: 7 * 24 * 60 * 60 * 1000,  // 7 DAYS
     });
 };
 
-export const signup = async (req, res) => {   // endpoint for signup
+export const signup = async (req, res) => {
     const { email, password, name } = req.body;  // collect details from frontend
 
     try {
@@ -60,9 +60,27 @@ export const signup = async (req, res) => {   // endpoint for signup
 };
 
 
-export const login = async (req, res) => {   // endpoint for Login
+export const login = async (req, res) => {
     res.send('LogIn route called');
-}
-export const logout = async (req, res) => {   // endpoint for Logout
-    res.send('LogOut route called');
-}
+};
+
+
+export const logout = async (req, res) => {
+    // While logout just del refreshToken from redis and clear the cookies
+    try {
+        const Token = req.cookies?.refreshToken;  // here refreshToken becoz we created cookie for refreshToken with this name
+        if (!Token) {
+            return res.status(401).json({ message: 'No refresh token found' });
+        }
+        // using verify function to decode the refreshToken
+        const decoded = jwt.verify(Token, process.env.REFRESH_TOKEN_SECRET);
+        await redis.del(`refresh_token:${decoded.userId}`);
+
+        res.clearCookie('accessToken');
+        res.clearCookie('refreshToken');
+        res.status(200).json({ message: 'Logout successfully' });
+    }
+     catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
